75113647cff9a55de6f368c75af4f1f38032707c
MBMAZAM <maza_1506@hotmail.com>
2023-06-18 11:07:00 -0500

    Avanzando

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// function tarea(done) {
+//     console.log("Mi primer tarea");
+
+//     done();
+// }
+
+// exports.tarea = tarea;
+
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require("gulp-plumber");
+
+function css(done){
+
+    src("src/scss/**/*.scss") //Identificar el archivo SASS
+        .pipe(plumber())
+        .pipe(sass()) //Compilarlo
+        .pipe(dest("build/css")); //Almacenarla en el disco duro
+    done(); //Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev(done) {
+    watch("src/scss/**/*.scss", css)
+    done();
+}
+
+
+exports.css = css;
+exports.dev = dev;
+
+
+
+
+
